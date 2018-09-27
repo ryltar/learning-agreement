c0a94e59823acf5c04ffab1322f2dae353497cf1
ryltar <mart1.guillaume@free.fr>
2018-09-27 22:21:50 +0200

    init

---
+++ src/main/java/com/fges/rizomm/m1iii/learningagreementAPI/LearningAgreementApiApplication.java
@@ -0,0 +1,12 @@
+package com.fges.rizomm.m1iii.learningagreementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAgreementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAgreementApiApplication.class, args);
+	}
+}
