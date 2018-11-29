7c4ac30feb1a8ccfc6f24e0d604fd795015939b5
vivienlonguet <vivien.longuet@lacatholille.fr>
2018-11-29 03:43:57 +0100

    Init module - component

---
+++ learning-agreement-FRONT/src/app/up/up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-up',
+  templateUrl: './up.component.html',
+  styleUrls: ['./up.component.css']
+})
+export class UpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
